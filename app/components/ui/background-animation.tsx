f9f5bd1b1360c3af4ea0f86db9a0a7c1ece62219
Kostia Novosydliuk <knovosyd@uwaterloo.ca>
2026-06-30 00:45:30 -0400

    Remove unused background animation import

--- app/components/ui/background-animation.tsx
+++ app/components/ui/background-animation.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef } from "react"
 import { animate, svg } from "animejs"
 import tracedSvgStr from "../../assets/traced-graphic.svg?raw"
 
